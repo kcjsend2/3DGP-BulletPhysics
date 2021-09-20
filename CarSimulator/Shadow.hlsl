d3cff410adeb2a8ecdcaacb4351cdf117be6fa0d
kcjsend2 <kcjsend2@naver.com>
2021-09-20 13:16:58 +0900

    Update Shadow.hlsl

--- CarSimulator/Shadow.hlsl
+++ CarSimulator/Shadow.hlsl
@@ -16,17 +16,10 @@ VertexOut VS(VertexIn vin)
 	
     // Transform to world space.
     float4 posW = mul(float4(vin.PosL, 1.0f), gmtxWorld);
-
+    // Transform to homogeneous clip space.
     
-    if(nShadowIndex == 1)
-    {
-        // Transform to homogeneous clip space.
-        vout.PosH = mul(posW, gmtxLightViewProj[nShadowIndex]);
-    }
-    else
-    {
-        vout.PosH = posW;
-    }
+    vout.PosH = mul(posW, gmtxLightViewProj[nShadowIndex]);
+    
     return vout;
 }
 
